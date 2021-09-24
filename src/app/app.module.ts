12045e7919d0efe866832301b3b2ecd7285d6b35
lf-lima <luizfelipe5007@gmail.com>
2021-09-24 12:24:46 -0300

    add angular material and bootstrap for style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { NewTransferComponent } from './new-transfer/new-transfer.component'
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module'
 import { ListTransfersComponent } from './list-transfers/list-transfers.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule  } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { NgxMaskModule } from 'ngx-mask'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { ListTransfersComponent } from './list-transfers/list-transfers.componen
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    NgxMaskModule.forRoot(),
+    FontAwesomeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
